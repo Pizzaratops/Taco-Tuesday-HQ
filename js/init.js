f4ac26f3550e73addd64d82119d23cacc2646569
Steakosaurus <93515926+Pizzaratops@users.noreply.github.com>
2026-05-12 09:56:24 +0200

    Update init.js

--- js/init.js
+++ js/init.js
@@ -10,10 +10,14 @@
     renderHome(); // always render home data in background
     _applyPage(initPageId);
     // Run page-specific init
-    if (initPageId === 'standingsPage') setTimeout(renderStandingsChart, 50);
-    if (initPageId === 'nbaTradesPage') renderNbaTrades();
+if (initPageId === 'standingsPage')     setTimeout(renderStandingsChart, 50);
+    if (initPageId === 'nbaTradesPage')     renderNbaTrades();
     if (initPageId === 'adminSettingsPage') _asInit();
-    if (initPageId === 'tradeHistoryPage') renderTradeHistory();
+    if (initPageId === 'tradeHistoryPage')  renderTradeHistory();
+    if (initPageId === 'draftboardPage')    showDraftboard();
+    if (initPageId === 'bestAvailPage')     showBestAvail();
+    if (initPageId === 'rankingsPage')      showRankings();
+    if (initPageId === 'analyticsPage')     showAnalytics();
   }
 })();
 initEspnSyncBtn();
